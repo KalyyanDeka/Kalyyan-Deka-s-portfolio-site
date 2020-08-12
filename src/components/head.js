45806e799dc1f11baf1e1cb457f7ae069e02c7cf
kalyyan <Kalyyan.2612@gmail.com>
2020-08-12 09:16:03 +0530

    modified the helmet

--- src/components/head.js
+++ src/components/head.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 const Head = () => {
-  return <Helmet title="KalyyanDeka | Portfolio" />
+  return <Helmet title="KalyyanDeka | Personal Site" />
 }
 
 export default Head
